ed85d29c7dccf8a7293d41400cd808393e808259
Dmytro Lambru <dlambru@geeksforless.net>
2019-05-14 19:09:01 +0300

    development started

--- lwc/util_Lookup/util_Lookup.js
+++ lwc/util_Lookup/util_Lookup.js
@@ -1,5 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class Util_Lookup extends LightningElement {
-    
+    @api isLabelHidden = false;
+    @track labelClass = 'slds-form-element__label';
+
+    constructor() {
+        super();
+        
+        this.defineLabelClass();
+    }
+
+    defineLabelClass() {        
+        this.isLabelHidden = true;
+        console.log(this.labelClass);
+
+        if (this.isLabelHidden) {
+            console.log(this.labelClass);
+            
+            this.labelClas = this.labelClas + ' slds-hide';
+            console.log(this.labelClass);
+        }
+    }
+
+    // defineLabelClass() {
+    //     let classString = 'slds-form-element__label';
+        
+    //     this.isLabelHidden = true;
+
+    //     if (this.isLabelHidden) {
+    //         classString += ' slds-hide';
+    //     }
+
+    //     return classString;
+    // }
 }
